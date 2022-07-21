0863a81703f3f9b26d51e5090d38b6024205d7bb
Wandia E Mugo <wwandia173@gmail.com>
2022-07-21 15:12:49 +0300

    Done

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
+// import { Route } from "react-router-dom";
+// import MoviesList from "./MoviesList.js";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route exact path={match.url}>
+      </Route>
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+        </Route>
+
     </div>
   );
 }
